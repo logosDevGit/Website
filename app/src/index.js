7df6414f5be9c1e5ce549836fab79e3e2bb761bc
JoaoVitorLima242 <jlima0500@gmail.com>
2022-04-18 10:28:49 -0300

    add favicons and adjusting global css

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './Views/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Assets/Styles/global.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
