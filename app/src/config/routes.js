536c9e5bf0d4886005272e915a97f971f0ae15fd
JoaoVitorLima242 <jlima0500@gmail.com>
2022-04-18 11:09:45 -0300

    create a navbar

---
+++ app/src/config/routes.js
@@ -0,0 +1,14 @@
+import App from "../views/App"
+import NavBar from "../components/NavBar";
+
+const Routes = () => {
+
+    return (
+        <>
+            <NavBar />
+            <App />
+        </>
+    )
+}
+
+export default Routes;
