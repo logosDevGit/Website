bddac69e07c43b3c18a6f011c0123098df0b5fb2
JoaoVitorLima242 <jlima0500@gmail.com>
2022-04-29 14:14:04 -0300

    adjsting heroSix

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -15,9 +15,9 @@ const Home = () => {
             <HeroThree />
             <HeroFour />
             <HeroFive/>
-            {/* <HeroSix/>
-            <HeroSeven/> */}
-        </>
+            <HeroSix/>
+{/*             <HeroSeven/>
+ */}        </>
     )
 
 }
