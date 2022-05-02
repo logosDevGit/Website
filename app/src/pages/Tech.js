ab7fc7a7d1b424e8276e41baa687f2189d179585
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-02 14:23:05 -0300

    set tech color

---
+++ app/src/pages/Tech.js
@@ -0,0 +1,27 @@
+import HeroOne from "../components/Hero/Home/HeroOne"
+import HeroTwo from "../components/Hero/Home/HeroTwo"
+import HeroThree from "../components/Hero/Home/HeroThree";
+import HeroFour from "../components/Hero/Home/HeroFour";
+import HeroFive from "../components/Hero/Home/HeroFive";
+import HeroSix from "../components/Hero/Home/HeroSix";
+import HeroSeven from '../components/Hero/Home/HeroSeven';
+import HeroEight from "../components/Hero/Home/HeroEight";
+
+const Tech = () => {
+    
+    return (
+        <>
+            <HeroOne />
+            <HeroTwo />
+            <HeroThree />
+            <HeroFour />
+            <HeroFive/>
+            <HeroSix/>
+            <HeroEight/>
+            <HeroSeven/>
+         </>
+    )
+
+}
+
+export default Tech;
